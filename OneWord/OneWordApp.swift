3a25f99434bbe696a4cab7ec71157981f52c5dd2
BenitzCoding <benitzcoding@yahoo.com>
2023-01-05 11:26:46 +0800

    Initial Commit

---
+++ OneWord/OneWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneWordApp.swift
+//  OneWord
+//
+//  Created by BenitzCoding on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct OneWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
